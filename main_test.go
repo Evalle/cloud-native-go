21bad882fa14af8b15e8b955d9c26661b2c218fe
Shmarnev, Evgeny <evgeny.shmarnev@sap.com>
2019-03-11 16:16:53 +0100

    u-test: custom port

--- main_test.go
+++ main_test.go
@@ -1,10 +1,23 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func TestPort(t *testing.T) {
+func TestDefaultPort(t *testing.T) {
 	got := port()
 	want := "8080"
+
+	if want != got {
+		t.Errorf("got %s, but want %s", got, want)
+	}
+}
+
+func TestCustomPort(t *testing.T) {
+	os.Setenv("PORT", "9090")
+	got := port()
+	want := "9090"
 
 	if want != got {
 		t.Errorf("got %s, but want %s", got, want)
